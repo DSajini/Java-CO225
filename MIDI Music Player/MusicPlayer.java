9a9a4b694a18db094a5bf272e91f54d390006def
Sajini Ishanthi <dsajini.i@gmail.com>
2019-12-25 08:54:54 +0530

    Initialized user input
    User input is taken as Command Line arguments.

--- MIDI Music Player/MusicPlayer.java
+++ MIDI Music Player/MusicPlayer.java
@@ -3,7 +3,7 @@ import javax.sound.midi.*;
 public class MusicPlayer{
 	
 	//play method
-	public void play(){
+	public void play(int instrumentValue,int noteValue){
 		try{
 			
 			//create a Sequencer (as a music device)
@@ -17,16 +17,26 @@ public class MusicPlayer{
 			//create a track
 			Track track = seq1.createTrack();
 			
+			MidiEvent event= null;
 			
-			//create midi event
+			//create midi event 
+			//change the instrument
 			ShortMessage a = new ShortMessage(); 
-			a.setMessage(144, 1, 20, 100); 
-			MidiEvent noteOn = new MidiEvent(a, 1); 
-			track.add(noteOn); 
+			a.setMessage(192, 1, instrumentValue, 0); 
+			MidiEvent setInstrument = new MidiEvent(a, 1); 
+			track.add(setInstrument); 
 			
+			
+			//start the note
 			ShortMessage b = new ShortMessage(); 
-			b. setMessage (144, 1, 102, 100); 
-			MidiEvent noteOff = new MidiEvent(b,16);
+			b. setMessage (144, 1, noteValue, 100); 
+			MidiEvent noteOn = new MidiEvent(b,1);
+			track.add(noteOn);
+			
+			//end the note
+			ShortMessage c = new ShortMessage(); 
+			c. setMessage (128, 1, noteValue, 100); 
+			MidiEvent noteOff = new MidiEvent(c,16);
 			track.add(noteOff);
 			
 			//set the sequence in the sequencer
@@ -45,7 +55,19 @@ public class MusicPlayer{
 	//main method
 	public static void main(String[] args){
 		MusicPlayer mini = new MusicPlayer();
-		mini.play();
+		
+		//Handle cmd line arguments
+		if(args.length<2){
+			System.out.println("Include both instrumet and note as arguments");
+		}
+		
+		else{
+			int instrumentValue=Integer.parseInt(args[0]);
+			int noteValue=Integer.parseInt(args[1]);
+			
+			
+			mini.play(instrumentValue,noteValue);
+		}
 	}
 	
 }
